b061502e3e109e0192ead81499450f743ecb52fb
Me <83992410+candycrusher149@users.noreply.github.com>
2021-09-16 21:09:47 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-var images = [
-  "https://i.postimg.cc/MGn9GJXw/family.jpg",
-  "https://i.postimg.cc/qqyYvVbq/grandpa.jpg", 
-  "https://i.postimg.cc/wjMnFtMX/father.jpg" , 
-  "https://i.postimg.cc/5ymDKL83/bro.jpg", 
-  "https://i.postimg.cc/JnL6wtrd/sister.jpg",
-   "https://i.postimg.cc/bw5W5zSK/mother.jpg"
-  ];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
-var i = 0;
-function update()
-{
-    i++;
-    var numbers_of_family_member_in_array = 5
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
+menu_list_array = ["Veg Margherita Pizza","pepporoni deluxe pizza","square pizza"
+                    ];
+
+function getmenu(){
+var htmldata = "";
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var imgtags = '<img id="im1" src="images/pizza.png"/>';
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
       }
-    
-    //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = images[i];
-    //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = names[i];
- 
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("family_member_name").innerHTML = updatedName;
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
